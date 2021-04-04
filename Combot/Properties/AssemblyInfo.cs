a477073bcec254c77bbe8379183cab61cd857701
Rohanyadav028 <81905748+Rohanyadav028@users.noreply.github.com>
2021-04-04 15:47:12 +0530

    Update AssemblyInfo.cs

--- Combot/Properties/AssemblyInfo.cs
+++ Combot/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Combot")]
+[assembly: AssemblyTitle("Mitsuri")]
 [assembly: AssemblyDescription("The friendly IRC Bot")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Teknik")]
